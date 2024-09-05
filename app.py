6e5f6dcb723c66900ac9243f152d6a4ae4418149
AlexSouzones <alexandresouzones@gmail.com>
2024-09-04 22:43:49 -0300

    remoção do resize do app

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ class App(ReverseScreen, FibonacciScreen, MonthlyIncomeScreen, GraphicScreen):
         self.root = root
         self.root.title("Teste Target Sistemas")
         self.root.geometry("800x600")
+        self.root.resizable(False, False)
         self.create_main_screen()
         self.setup_ui()
         
